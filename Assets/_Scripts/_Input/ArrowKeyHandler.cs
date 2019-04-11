2e7943bf3d44f5d16eb985f51795565c59400579
nicholas mcdonald <nmcdonald@dal.ca>
2019-04-10 22:52:50 -0300

    Extract arrow key handling into its own class

---
+++ Assets/_Scripts/_Input/ArrowKeyHandler.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowKeyHandler
+{
+    private Cursor cursor;
+
+    private const float SCROLL_START_TIME = 0.2f;
+    private const float SCROLL_CONTINUE_TIME = 0.05f;
+
+    private float arrowKeyActivationTime;
+    private bool arrowKeyHeld;
+
+    public ArrowKeyHandler(Cursor cursor)
+    {
+        this.cursor = cursor;
+    }
+
+    public void DoStuff(Dictionary<KeyCode, KeyState> inputs)
+    {
+        if (!CheckAllArrowsReleased(inputs) || arrowKeyHeld)
+            HandleArrows(inputs);
+    }
+
+    void HandleArrows(Dictionary<KeyCode, KeyState> inputs)
+    {
+        // On key release stop scroll and reset rate limiter
+        if (CheckAllArrowsReleased(inputs))
+        {
+            arrowKeyHeld = false;
+            arrowKeyActivationTime = 0.0f;
+            return;
+        }
+
+        // Ignore key inputs received between rate intervals
+        if (arrowKeyActivationTime > Time.time)
+            return;
+
+        int verticalMovement = GetInputAxisValue(inputs[KeyCode.S], inputs[KeyCode.W]);
+        int horizontalMovement = GetInputAxisValue(inputs[KeyCode.A], inputs[KeyCode.D]);
+
+        // Change the rate limiter based on whether scrolling has already started
+        if (arrowKeyHeld)
+        {
+            arrowKeyActivationTime = Time.time + SCROLL_CONTINUE_TIME;
+        }
+        else
+        {
+            arrowKeyActivationTime = Time.time + SCROLL_START_TIME;
+            arrowKeyHeld = true;
+        }
+
+        cursor.Move(new Vector2Int(horizontalMovement, verticalMovement));
+    }
+
+    private bool CheckAllArrowsReleased(Dictionary<KeyCode, KeyState> inputs)
+    {
+        var arrowKeys = new List<KeyCode> { KeyCode.W, KeyCode.A, KeyCode.S, KeyCode.D };
+        foreach (KeyCode arrow in arrowKeys)
+        {
+            if (inputs[arrow] != KeyState.Released && inputs[arrow] != KeyState.None)
+                return false;
+        }
+
+        return true;
+    }
+
+    private int GetInputAxisValue(KeyState negative, KeyState positive)
+    {
+        if (positive == KeyState.Pressed || positive == KeyState.Held)
+            return 1;
+        else if (negative == KeyState.Pressed || negative == KeyState.Held)
+            return -1;
+        else
+            return 0;
+    }
+}
--- Assets/_Scripts/_Input/MapControlState.cs
+++ Assets/_Scripts/_Input/MapControlState.cs
@@ -8,8 +8,7 @@ public class MapControlState : MonoBehaviour, ControlObserver
     private const float SCROLL_START_TIME = 0.2f;
     private const float SCROLL_CONTINUE_TIME = 0.05f;
 
-    private float arrowKeyActivationTime;
-    private bool arrowKeyHeld;
+    private ArrowKeyHandler arrowKeyHandler;
     private MapActionState currentSelectionState;
     private List<MapActionStateObserver> selectionStateObservers;
 
@@ -18,12 +17,12 @@ public class MapControlState : MonoBehaviour, ControlObserver
         GetComponent<InputManager>().ControlState = this;
         currentSelectionState = MapActionState.NoSelection;
         selectionStateObservers = new List<MapActionStateObserver>();
+        arrowKeyHandler = new ArrowKeyHandler(cursor);
     }
 
     public void Notify(Dictionary<KeyCode, KeyState> inputs)
     {
-        if (!CheckAllArrowsReleased(inputs) || arrowKeyHeld)
-            HandleArrows(inputs);
+        arrowKeyHandler.DoStuff(inputs);
         if (inputs[KeyCode.Return] == KeyState.Held)
             SetActionState(MapActionState.NoSelection);
     }
@@ -33,59 +32,6 @@ public class MapControlState : MonoBehaviour, ControlObserver
         selectionStateObservers.Add(subscriber);
     }
 
-    void HandleArrows(Dictionary<KeyCode, KeyState> inputs)
-    {
-        // On key release stop scroll and reset rate limiter
-        if (CheckAllArrowsReleased(inputs))
-        {
-            arrowKeyHeld = false;
-            arrowKeyActivationTime = 0.0f;
-            return;
-        }
-
-        // Ignore key inputs received between rate intervals
-        if (arrowKeyActivationTime > Time.time)
-            return;
-
-        int verticalMovement = GetInputAxisValue(inputs[KeyCode.S], inputs[KeyCode.W]);
-        int horizontalMovement = GetInputAxisValue(inputs[KeyCode.A], inputs[KeyCode.D]);
-
-        // Change the rate limiter based on whether scrolling has already started
-        if (arrowKeyHeld)
-        {
-            arrowKeyActivationTime = Time.time + SCROLL_CONTINUE_TIME;
-        }
-        else
-        {
-            arrowKeyActivationTime = Time.time + SCROLL_START_TIME;
-            arrowKeyHeld = true;
-        }
-
-        cursor.Move(new Vector2Int(horizontalMovement, verticalMovement));
-    }
-
-    private bool CheckAllArrowsReleased(Dictionary<KeyCode, KeyState> inputs)
-    {
-        var arrowKeys = new List<KeyCode> { KeyCode.W, KeyCode.A, KeyCode.S, KeyCode.D };
-        foreach (KeyCode arrow in arrowKeys)
-        {
-            if (inputs[arrow] != KeyState.Released && inputs[arrow] != KeyState.None)
-                return false;
-        }
-
-        return true;
-    }
-
-    private int GetInputAxisValue(KeyState negative, KeyState positive)
-    {
-        if (positive == KeyState.Pressed || positive == KeyState.Held)
-            return 1;
-        else if (negative == KeyState.Pressed || negative == KeyState.Held)
-            return -1;
-        else
-            return 0;
-    }
-
     private void SetActionState(MapActionState actionState)
     {
         currentSelectionState = actionState;
@@ -93,11 +39,4 @@ public class MapControlState : MonoBehaviour, ControlObserver
             observer.ChangeSelectionState(currentSelectionState);
     }
 }
-
-
-// The 'cursor' for this state contains a coordinate reference to the map.
-// It interfaces with the NavMap to move around
-
-
-// 
    
