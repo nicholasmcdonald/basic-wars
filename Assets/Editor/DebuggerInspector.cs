309b6dae1e1643ca634249fbea5236443bbcf3cf
nicholas mcdonald <nmcdonald@dal.ca>
2019-04-19 16:57:01 -0300

    Add custom inspector to beef up the Debugger; can spawn units through inspector

---
+++ Assets/Editor/DebuggerInspector.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Debugger))]
+public class DebuggerInspector : Editor
+{
+    bool prefabWindowOpen = false;
+    bool spawnWindowOpen = false;
+
+    public override void OnInspectorGUI()
+    {
+        Debugger debugger = target as Debugger;
+
+        prefabWindowOpen = EditorGUILayout.Foldout(prefabWindowOpen, "Prefabs");
+        if (prefabWindowOpen)
+            DrawDefaultInspector();
+
+        spawnWindowOpen = EditorGUILayout.Foldout(spawnWindowOpen, "Spawn");
+        if (spawnWindowOpen)
+        {
+            if (GUILayout.Button("Spawn infantry"))
+                debugger.Spawn(debugger.infantry);
+            if (GUILayout.Button("Spawn helicopter"))
+                debugger.Spawn(debugger.helicopter);
+            if (GUILayout.Button("Spawn battleship"))
+                debugger.Spawn(debugger.battleship);
+        }
+    }
+}
--- Assets/_Scripts/Debugger.cs
+++ Assets/_Scripts/Debugger.cs
@@ -6,32 +6,8 @@ public class Debugger : MonoBehaviour
     public GameObject battleship;
     public GameObject helicopter;
 
-    private void Update()
+    public void Spawn(GameObject prefab)
     {
-        Vector2Int target = GameObject.FindGameObjectWithTag("Player").GetComponent<Cursor>().Position;
-
-        if (Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            Unit.Spawn(infantry, target);
-            Debug.Log("Attempting to spawn infantry...");
-        }
-
-        if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            Unit.Spawn(battleship, target);
-            Debug.Log("Attempting to spawn battleship...");
-        }
-
-        if (Input.GetKeyDown(KeyCode.Alpha3))
-        {
-            Unit.Spawn(helicopter, target);
-            Debug.Log("Attempting to spawn helicopter...");
-        }
-
-        if (Input.GetKeyDown(KeyCode.Alpha0))
-        {
-            Map map = GameObject.FindGameObjectWithTag("Map").GetComponent<Map>();
-            Debug.Log(map.GetMapTileAt(target).WorldPosition);
-        }
+        Unit.Spawn(prefab, GameObject.FindGameObjectWithTag("Player").GetComponent<Cursor>().Position);
     }
 }
